74e10d41a9f8b322990506688bc569c345647730
Bentley Settin <bfs7357@uncw.edu>
2024-01-26 08:17:16 -0500

    AddedCode

--- Firmware/src/TinyCamPublish.ino
+++ Firmware/src/TinyCamPublish.ino
@@ -1,15 +1,4 @@
 
-SYSTEM_THREAD(ENABLED);
-SYSTEM_MODE(SEMI_AUTOMATIC);
-
-SystemSleepConfiguration config;
-
-// Define whether (1) or not (0) to publish
-#define PUBLISHING 0
-
-// Global objects
-FuelGauge batteryMonitor;
-const char * eventName = "FloodorNo";
 
 //------------------State variables
 // not yet used but placeholders in case of additional states
@@ -20,11 +9,22 @@ enum State {
 };
 State state = DATALOG_STATE;
 
-// Define whether (1) or not (0) to publish (PJB: redefining to 0 for initial testing sans cell)
+// Define whether (1) or not (0) to publish
 #define PUBLISHING 0
 
 unsigned long stateTime = 0;
 char data[120];
+
+SYSTEM_MODE(MANUAL);
+SYSTEM_THREAD(ENABLED);
+//SYSTEM_MODE(SEMI_AUTOMATIC);
+
+
+// Global objects
+FuelGauge batteryMonitor;
+const char * eventName = "FloodorNo";
+
+SystemSleepConfiguration config;
 
 // Various timing constants
 //const unsigned long MAX_TIME_TO_PUBLISH_MS = 20000; // Only stay awake for this time trying to connect to the cloud and publish
@@ -69,23 +69,19 @@ void loop(void) {
 
             delay(1000);
 
-            Serial.println(Serial1.readString()); //PJB: this doesn't "return" the answer, it just prints it to serial monitor. Therefore you won't be able to access it for publishing.
+            //Serial.println(Serial1.readString());
 
         //delay(3000);
 
             digitalWrite(A0, HIGH);
 
-          //PJB: move sleep code to your sleep state.
-            config.mode(SystemSleepMode::ULTRA_LOW_POWER)
-            .gpio(A0, FALLING)
-            .duration(60* 1000L); // Set seconds until wake    
+            
         }
 
     float cellVoltage = batteryMonitor.getVCell();
     float stateOfCharge = batteryMonitor.getSoC();
     real_time = Time.now();
 
-      // PJB: try to predict what this will save to the data variable. Is a "flood" or "no flood" response anywhere here? 
     snprintf(data, sizeof(data), "%li,%.5f,%.02f,%.02f", //,%.5f,%.5f,%.5f,%.5f,%.5f,%.02f,%.02f",
       real_time, // if it takes a while to connect, this time could be offset from sensor recording
       cellVoltage, stateOfCharge
@@ -149,12 +145,13 @@ void loop(void) {
 
 //Ready to sleep
     case SLEEP_STATE:{
-      // PJB: move your config details from above down to here
-        SystemSleepResult result = System.sleep(config); // Device sleeps here
+        config.mode(SystemSleepMode::ULTRA_LOW_POWER)
+            .gpio(A0, FALLING)
+            .duration(60* 1000L); // Set seconds until wake    
     
     }
         
-//PJB: integrate this soon. You currently have your sleep timing hard-coded. The following code will give you more robust timing.
+
      //int secondsUntilNextEvent() {
 
          //int current_seconds = Time.now();
